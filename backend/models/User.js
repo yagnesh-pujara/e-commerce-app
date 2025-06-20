5a1e4c13244b6d5ac944b93f6c4f9e6111394bff
Yagnesh Pujara <yagnesh.pujara129247@marwadiuniversity.ac.in>
2025-06-20 11:49:15 +0530

    User Model Creation

---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = { User };
